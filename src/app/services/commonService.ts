530add9b9176bf1b4d622929929ff2ad4167b274
S. Yousuf Hussain Abidi <yousuf.abidi@inseyab.com>
2025-02-26 17:36:27 +0500

    get produtc post API added

--- src/app/services/commonService.ts
+++ src/app/services/commonService.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class CommonService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   getAll(): Observable<any> {
     return this.httpClient.get<any>(`${environment.apiUrl}/Category/GetAll`);
@@ -26,35 +26,33 @@ export class CommonService {
     return this.httpClient.get<any>(`${environment.apiUrl}/Product/GetProductBanner`);
   }
 
-  getProducts(
-    pageSize: number,
-    pageNumber: number,
-    categoryId?: number,
-    type?: number,
-    name: string = '',
-    hashTag: string = '',
-    fromPrice?: number,
-    toPrice?: number,
-    size?: string
-  ): Observable<any> {
-    let params = new HttpParams()
-      .set('pageSize', pageSize.toString())
-      .set('pageNumber', pageNumber.toString());
-
-    if (categoryId !== undefined) params = params.set('categoryId', categoryId.toString());
-    if (type !== undefined) params = params.set('type', type.toString());
-    if (name) params = params.set('name', name);
-    if (hashTag) params = params.set('hashTag', hashTag);
-    if (fromPrice !== undefined) params = params.set('fromPrice', fromPrice.toString());
-    if (toPrice !== undefined) params = params.set('toPrice', toPrice.toString());
-    if (size) params = params.set('size', size);
-
-    return this.httpClient.get<any>(`${environment.apiUrl}/Product/GetProducts`, { params });
-  }
-  
   getBestSeller(): Observable<any> {
     return this.httpClient.get<any>(`${environment.apiUrl}/Product/GetBestSellingProducts`);
   }
 
-  
+  getProducts(
+    pageSize: number,
+    pageNumber: number,
+    categoryId: number,
+    type: number,
+    name: string = '',
+    hashTag: string = '',
+    fromPrice?: number,
+    toPrice?: number,
+    size?: string): Observable<any> {
+      var formData = new FormData();
+      formData.append('PageSize', pageSize.toString())
+      formData.append('PageNumber', pageNumber.toString())
+      formData.append('CategoryId', categoryId.toString())
+      formData.append('Type', type.toString())
+      formData.append('Name', name)
+      formData.append('HashTag', hashTag)
+      formData.append('FromPrice', fromPrice?.toString() ?? '')
+      formData.append('ToPrice', toPrice?.toString() ?? '')
+      formData.append('Size', size?.toString() ?? '')
+
+      return this.httpClient.post<any>(`${environment.apiUrl}/Product/GetProducts`, formData );
+  }
+
+
 }
