18c8be58df569b35844769396393d1f93d1a44e2
Yousuf Abidi <yousufhussainabidi@gmail.com>
2025-03-10 23:56:36 +0500

    producttion env added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.staging.ts
 export const environment = {
-    production: false,
-    apiUrl: 'https://localhost:7197/api',
+    production: true,
+    apiUrl: 'https://mjaydt.com/mjClothingBackend/api',
   };
   
