cc293ab40a2cfd2a3f279a224590418470f3fb63
S. Yousuf Hussain Abidi <yousuf.abidi@inseyab.com>
2025-03-12 10:38:22 +0500

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 // src/environments/environment.staging.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://mjaydt.com/mjClothingBackend/api',
+    // apiUrl: 'https://mjaydt.com/mjClothingBackend/api',
+    apiUrl: '/api',
   };
   
